d712f5e3255128417303b9e137bf1a64631e212e
carmi <carmi@CERMINA_MAGED>
2025-02-14 20:04:49 +0200

    Join Operators -GroupJoin()

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Demo.Data;
 using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using System.Data.Entity;
+using System.Reflection.Emit;
 
 namespace Demo
 {
@@ -158,39 +161,115 @@ namespace Demo
             #region LINQ JOin Operators [Join() , GroupJoin() ]
 
             #region inner Join 
-           /// var Result = from D in dbContext.Departments
-           ///              join E in dbContext.Employees
-           ///              on D.DeptId equals E.DepartmentDeptId
-           ///              select new
-           ///              {
-           ///                  EmployeeId = E.Code,
-           ///                  EmployeeName = E.Name,
-           ///                  DepartmentId = D.DeptId,
-           ///                  DepartmentName = D.Name
-           ///              };
+            /// var Result = from D in dbContext.Departments
+            ///              join E in dbContext.Employees
+            ///              on D.DeptId equals E.DepartmentDeptId
+            ///              select new
+            ///              {
+            ///                  EmployeeId = E.Code,
+            ///                  EmployeeName = E.Name,
+            ///                  DepartmentId = D.DeptId,
+            ///                  DepartmentName = D.Name
+            ///              };
 
-          ///  Result = dbContext.Departments.Join(dbContext.Employees,
-          ///                                      D => D.DeptId,
-          ///                                      E => E.DepartmentDeptId,
-          ///                                      (D, E) => new
-          ///                                      {
-          ///                                          EmployeeId = E.Code,
-          ///                                          EmployeeName = E.Name,
-          ///                                          DepartmentId = D.DeptId,
-          ///                                          DepartmentName = D.Name
-          ///
-          ///                                      });
-          ///                  
+            ///  Result = dbContext.Departments.Join(dbContext.Employees,
+            ///                                      D => D.DeptId,
+            ///                                      E => E.DepartmentDeptId,
+            ///                                      (D, E) => new
+            ///                                      {
+            ///                                          EmployeeId = E.Code,
+            ///                                          EmployeeName = E.Name,
+            ///                                          DepartmentId = D.DeptId,
+            ///                                          DepartmentName = D.Name
+            ///
+            ///                                      });
+            ///                  
 
-         ///  foreach (var item in Result)
-         ///  {
-         ///      Console.WriteLine($"Employee : {item.EmployeeName}  Department : {item.DepartmentName}");
-         ///  }
+            ///  foreach (var item in Result)
+            ///  {
+            ///      Console.WriteLine($"Employee : {item.EmployeeName}  Department : {item.DepartmentName}");
+            ///  }
 
 
             #endregion
 
+            #region GroupJoin 
 
+            #region Example 01 
+            /// var Result = dbContext.Departments.GroupJoin(dbContext.Employees
+            ///                                       , D => D.DeptId,
+            ///                                       E => E.DepartmentDeptId,
+            ///                                       (Department, Employees) => new
+            ///                                       {
+            ///                                           Department,
+            ///                                           Employees
+            ///
+            ///                                       }).Where(X => X.Employees.Count() > 0);//Where used to make innner JOin
+            ///
+
+            ///  Result = from D in dbContext.Departments
+            ///           join E in dbContext.Employees
+            ///           on D.DeptId equals E.DepartmentDeptId
+            ///           into emps
+            ///           select new
+            ///           {
+            ///               Department = D,
+            ///               Employees = emps
+            ///
+            ///           } into X
+            ///           where X.Employees.Count() > 0
+            ///           select X;
+
+
+            ///  foreach(var item in Result)
+            ///  {
+            ///      Console.WriteLine($"Department : Id = {item.Department.DeptId}  Name = {item.Department.Name}");
+            ///      foreach(var employee in item.Employees)
+            ///      {
+            ///          Console.WriteLine($"...Employees : Code :{employee.Code} Name :{employee.Name} ");
+            ///      }
+            ///  }
+            /// 
+            #endregion
+
+            #region Example 02
+
+            /// var Result = dbContext.Employees.GroupJoin(dbContext.Departments,
+            ///                                      E => E.DepartmentDeptId,
+            ///                                      D => D.DeptId,
+            ///                                      (Employee, Departments) => new
+            ///                                      {
+            ///                                          Employee,
+            ///                                          Departments
+            ///
+            ///                                      }).Where(X => X.Departments.Count() > 0);//Where used to make innner JOin
+            ///
+
+
+            ///  Result = from E in dbContext.Employees
+            ///          join D in dbContext.Departments
+            ///        on E.DepartmentDeptId equals D.DeptId
+            ///        into depts
+            ///          select new
+            ///          {
+            ///              Employee = E,
+            ///              Departments = depts
+            ///
+            ///          } into X
+            ///          where X.Departments.Count() > 0
+            ///          select X;
+
+
+            /// foreach(var item in Result)
+            /// {
+            ///     Console.WriteLine($"Employee : Code = {item.Employee.Code}  Name = {item.Employee.Name}");
+            ///     foreach(var departments in item.Departments)    
+            ///         Console.WriteLine($"...Department : Id = {departments.DeptId}  Name ={departments.Name}");
+            /// }
+
+
+            #endregion
+            #endregion
 
             #endregion
 
