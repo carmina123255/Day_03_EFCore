5082477425186ec0cab7e80b20e8bea6dfcb5fca
carmi <carmi@CERMINA_MAGED>
2025-02-14 14:13:51 +0200

    Navigational Properties

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,7 +7,46 @@ namespace Demo
         static void Main(string[] args)
         {
            using  CompanyDbContext dbContext = new CompanyDbContext();
-          CompanDbContextSeed.seed(dbContext);  
+
+            #region Loading Navigation Properties 
+
+            #region Navigation Property By Default Isn't Loeaded 
+            #region Example01 
+
+            // CompanDbContextSeed.seed(dbContext);  
+
+            ///  var employee = (from e in dbContext.Employees
+            ///                 where e.Code == 6
+            ///                 select e).FirstOrDefault();
+            ///
+            ///  if(employee is not null)
+            ///  {
+            ///      Console.WriteLine($"Employee :Id {employee.Code} :: Name ={employee.Name} :: \n Department : {employee.Department?.Name ?? "Not Found "} ");
+            ///  }
+
+            #endregion
+
+            #region Example 02 
+            ///   var department =( from d in dbContext.Departments
+            ///                    where d.DeptId==10
+            ///                    select d).FirstOrDefault();
+            ///
+            ///
+            ///   if (department is not null)
+            ///   {
+            ///       Console.WriteLine($"Department : Id = {department.DeptId} :: Name =  {department.Name}");
+            ///
+            ///       foreach(var item in department.Employees)
+            ///           Console.WriteLine($"---Employee : Name {item.Name}");
+            ///
+            ///   }
+            /// 
+            #endregion  
+            #endregion
+
+
+            #endregion
+
         }
     }
 }
